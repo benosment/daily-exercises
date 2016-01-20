538cb40d99378064c978b051438d9ddbc41fe9b5
Benjamin Osment <benjamin.j.osment@gmail.com>
2016-01-20 06:25:17 -0500

    added easy 248

--- easy-249.cc
+++ easy-249.cc
@@ -1,41 +1,39 @@
 #include <iostream>
 #include <vector>
-#include <sstream>
+#include <fstream>
 #include <string>
 
 using namespace std;
 
-
 int main() {
-  string raw_input;
-  string token;
-  vector<double> stock_prices;
-  cout << "Enter stock prices: ";
-  getline(cin, raw_input);
-  cout << "Raw input:" << raw_input << endl;
-  istringstream iss(raw_input);
-  int buy_day, sell_day, max_buy, max_sell = 0;
-  double profit, max_profit = 0;
+	string token;
+	vector<double> stock_prices;
+	ifstream inputfile("easy-249.txt");
+	int buy_day, sell_day, max_buy, max_sell = 0;
+	double profit, max_profit = 0;
 
-  while(iss) {
-    iss >> token;
-    if (!token.empty() && iss) {
-      stock_prices.push_back(stod(token));
-    }
-  }
+	if (!inputfile.is_open()) {
+		cout << "Could not open easy-249.txt" << endl;
+		return -1;
+	}
 
-  for (buy_day = 0; buy_day < stock_prices.size(); buy_day++) {
-    for (sell_day = buy_day + 2; sell_day < stock_prices.size(); sell_day++) {
-      profit = stock_prices[sell_day] - stock_prices[buy_day];
-      if (profit > max_profit) {
-        max_profit = profit;
-        max_buy = buy_day;
-        max_sell = sell_day;
-      }
-    }
-  }
+	while (inputfile >> token) {
+		stock_prices.push_back(stod(token));
+	}
+	inputfile.close();
 
-  cout << "Max profit " << max_profit << " from buy on day " << max_buy << " at " << stock_prices[max_buy] << " and sell on day " << max_sell << " at " << stock_prices[max_sell] << endl;
+	for (buy_day = 0; buy_day < stock_prices.size(); buy_day++) {
+		for (sell_day = buy_day + 2; sell_day < stock_prices.size(); sell_day++) {
+			profit = stock_prices[sell_day] - stock_prices[buy_day];
+			if (profit > max_profit) {
+				max_profit = profit;
+				max_buy = buy_day;
+				max_sell = sell_day;
+			}
+		}
+	}
 
-  return 0;
+	cout << "Max profit " << max_profit << " from buy on day " << max_buy << " at " << stock_prices[max_buy] << " and sell on day " << max_sell << " at " << stock_prices[max_sell] << endl;
+
+	return 0;
 }
